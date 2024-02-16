38b1ab1b9febd958f06c01a7e8090895ac3faaba
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2024-02-16 12:34:16 -0300

    :sparkles:feat
    :sparkles:feat criado método buscar por id

--- src/main/java/com/example/crud/controllers/ProdutoController.java
+++ src/main/java/com/example/crud/controllers/ProdutoController.java
@@ -1,12 +1,15 @@
 package com.example.crud.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,9 +36,21 @@ public class ProdutoController {
     }
 
     // Método para Listar os produtos
-    @GetMapping("produtos")
+    @GetMapping("/produtos")
     public ResponseEntity<List<ProdutoModel>> listarTodos() {
         return ResponseEntity.status(HttpStatus.OK).body(produtosRepository.findAll());
     }
+
+    // Método para Listar o produto por id
+    @GetMapping("/produtos/{id}")
+    public ResponseEntity<Object> buscarPorId(@PathVariable(value = "id") UUID id){
+        Optional<ProdutoModel> produto0 = produtosRepository.findById(id);
+        if(produto0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(produto0.get());
+    }
+
+
 
 }
